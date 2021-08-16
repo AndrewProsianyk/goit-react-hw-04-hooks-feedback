f53cde06b483885e422f2edd19a5e122f442fc1b
Andrew <nonameznoone@gmail.com>
2021-08-16 22:20:59 +0300

    complete

--- src/App.js
+++ src/App.js
@@ -1,52 +1,63 @@
-import React, {Component} from 'react';
+import {useState, useEffect} from 'react';
 import FeedbackOptions from './Feedback/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
 import Section from './Section/Section';
 
 
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
-  }
-  
-  onLeaveFeedback = option => {
-    this.setState(prevState => ({
-        [option]: prevState[option] + 1
-    }))
-  }
-  countTotalFeedback = () => {
-    return this.state.good + this.state.neutral + this.state.bad;
-  }
-  
-  countPositiveFeedbackPercentage = () => {
-    return (this.state.good * 100 / (this.countTotalFeedback())).toFixed(0)
-  }
+function App() {
+
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [goodFbPercent, setGoodFbPercent]=useState(0)
   
 
-  render()
-  {
-      return (
-        <div>
-          <Section title="Please leave feedback">
-            <FeedbackOptions
-              options={['good', 'neutral', 'bad']} 
-              onLeaveFeedback={this.onLeaveFeedback} />
-          </Section>
-
-          <Section title="Statistics">
-            <Statistics
-            good={this.state.good} 
-            neutral={this.state.neutral} 
-            bad={this.state.bad} 
-            total={this.countTotalFeedback()} 
-            positivePercentage={this.countPositiveFeedbackPercentage()} />
-          </Section>
-          
-        </div>
-      )
+  const feedbackCountUpdate = (type) => {
+    switch (type) {
+      case 'good':
+        setGood(prevState=>prevState+1);
+        break
+      case 'neutral':
+        setNeutral(prevState=>prevState+1);
+        break
+      case 'bad':
+        setBad(prevState=>prevState+1);
+        break
+      default:
+        return
     }
   }
+  useEffect(() => {
+    setTotal(good + bad + neutral)
+  },[good, bad, neutral])
+
+  useEffect(() => {
+    setGoodFbPercent((good * 100 / total).toFixed(0))
+  },[good, total])
+ 
+    return (
+      <div>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={['good', 'neutral', 'bad']}
+            onLeaveFeedback={feedbackCountUpdate}
+            />
+        </Section>
+
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={goodFbPercent}
+          />
+        </Section>
+          
+      </div>
+    )
+  
+}
 
 export default App;
